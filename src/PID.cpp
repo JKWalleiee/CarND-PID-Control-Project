e05e7fa8ada82fbe76b41e5855048fa6c56f765c
JKWalleiee <jhonra55@hotmail.com>
2019-06-21 02:35:49 +0000

    Implementation of the PID controller

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,4 +1,5 @@
 #include "PID.h"
+#include <algorithm>
 
 /**
  * TODO: Complete the PID class. You may add any additional desired functions.
@@ -9,22 +10,38 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp_, double Ki_, double Kd_) {
-  /**
-   * TODO: Initialize PID coefficients (and errors, if needed)
-   */
+  // Initialize PID coefficients (and errors, if needed)
+  Kp = Kp_;
+  Ki = Ki_;
+  Kd = Kd_;
+
+  p_error = 0.0;
+  i_error = 0.0;
+  d_error = 0.0;
+
+  // Previous cte.
+  prev_cte = 0.0;
 
 }
 
 void PID::UpdateError(double cte) {
-  /**
-   * TODO: Update PID errors based on cte.
-   */
+  /// Update PID errors based on cte.
+  
+  // Proportional error.
+  p_error = cte;
+
+  // Integral error.
+  i_error += cte;
+
+  // Diferential error.
+  d_error = cte - prev_cte;
+  
+  // save actual cte
+  prev_cte = cte;
 
 }
 
 double PID::TotalError() {
-  /**
-   * TODO: Calculate and return the total error
-   */
-  return 0.0;  // TODO: Add your total error calc here!
+  // Calculate and return the total error
+  return p_error * Kp + i_error * Ki + d_error * Kd;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -34,9 +34,8 @@ int main() {
   uWS::Hub h;
 
   PID pid;
-  /**
-   * TODO: Initialize the pid variable.
-   */
+   //Initialize the pid variable.
+  pid.Init(0.15, 0.0, 4.0);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, 
                      uWS::OpCode opCode) {
@@ -57,12 +56,10 @@ int main() {
           double speed = std::stod(j[1]["speed"].get<string>());
           double angle = std::stod(j[1]["steering_angle"].get<string>());
           double steer_value;
-          /**
-           * TODO: Calculate steering value here, remember the steering value is
-           *   [-1, 1].
-           * NOTE: Feel free to play around with the throttle and speed.
-           *   Maybe use another PID controller to control the speed!
-           */
+          
+          //Calculate steering value here
+          pid.UpdateError(cte);
+          steer_value = -pid.TotalError();
           
           // DEBUG
           std::cout << "CTE: " << cte << " Steering Value: " << steer_value 
